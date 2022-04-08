eee506314c05c80a50926e907fdf230883e512f3
zübeyir <zeynelzatari@gmail.com>
2022-04-08 07:55:54 +0200

    started test case 002 step 3 is done

--- src/us100/TesCase002.java
+++ src/us100/TesCase002.java
@@ -2,10 +2,14 @@ package us100;
 
 public class TesCase002 {
 
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
     // step 1 is done
 
 
-    // step 2 is done
+    // step 2 is done   zutztuztzut
 
     //step 3 is done
 
